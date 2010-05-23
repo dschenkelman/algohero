4b6f62e583b67655cb80d03cc962c03514e6648d
schenkelmania <schenkelmania@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-05-23 22:24:01 +0000

    Agregue funcionalidad para guardar parametros del constructor.

---  algohero --username schenkelmania/Source/AlgoHeroEntities/TiempoCancion.cs
+++  algohero --username schenkelmania/Source/AlgoHeroEntities/TiempoCancion.cs
@@ -7,15 +7,28 @@ namespace AlgoHero.Entities
     {
         public TiempoCancion(double duracionCompas, int cantidadBlancas)
         {
-                if (duracionCompas <= 0)
-                {
-                    throw new ArgumentException(Resources.DuracionCompasIncorrecta);
-                }
+            if (duracionCompas <= 0)
+            {
+                throw new ArgumentException(Resources.DuracionCompasIncorrecta);
+            }
 
-                if (cantidadBlancas <= 0)
-                {
-                    throw new ArgumentException(Resources.DuracionCompasIncorrecta);
-                }
+            if (cantidadBlancas <= 0)
+            {
+                throw new ArgumentException(Resources.DuracionCompasIncorrecta);
+            }
+
+            this.DuracionCompas = duracionCompas;
+            this.CantidadBlancas = cantidadBlancas;
+        }
+
+        public double DuracionCompas
+        {
+            get; private set;
+        }
+
+        public int CantidadBlancas
+        {
+            get; private set;
         }
     }
 }
