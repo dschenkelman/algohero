9fb327180bb0ca80e68670ae07bc95a9db906cde
schenkelmania <schenkelmania@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-05-29 21:40:36 +0000

    Agregue funcionalidad partitura para agregarle compases

--- Source/AlgoHeroEntities.Tests/Core/PartituraFixture.cs
+++ Source/AlgoHeroEntities.Tests/Core/PartituraFixture.cs
@@ -1,14 +1,63 @@
-﻿namespace AlgoHeroMusic.Entities.Tests.Core
+﻿using AlgoHero.MusicEntities.Core;
+using AlgoHero.MusicEntities.Enums;
+
+namespace AlgoHeroMusic.Entities.Tests.Core
 {
     using NUnit.Framework;
+    using AlgoHero.MusicEntities.Excepciones;
 
     [TestFixture]
     public class PartituraFixture
     {
+        private Compas compasCompleto;
+        private Compas compasIncompleto;
+        private Partitura partitura;
+        
+        [SetUp]
+        public void TestInitialize()
+        {
+            var tiempoCancion = new TiempoCancion(4, 2);
+            this.CrearCompasCompleto(tiempoCancion);
+            this.partitura = new Partitura(tiempoCancion);
+        }
+
+        [Test]
+        [ExpectedException(ExceptionType = typeof(ExcepcionCompasInvalido))]
+        public void AgregarCompasAPartituraNoAgregaCompasIncompleto()
+        {
+            this.partitura.AgregarCompas(this.compasIncompleto);
+        }
+
         [Test]
         public void AgregarCompasAPartituraAgregaCompasCompleto()
         {
+            this.partitura.AgregarCompas(this.compasCompleto);
 
+            Assert.AreEqual(1, this.partitura.CantidadCompases);
+            Assert.AreEqual(this.compasCompleto, this.partitura.ObtenerCompas(0));
+        }
+
+
+        private void CrearCompasCompleto(TiempoCancion tiempoCancion)
+        {
+            this.compasCompleto = new Compas(tiempoCancion);
+            var nota = new Nota(Tono.Fa, FiguraMusical.Negra);
+            var nota2 = new Nota(Tono.Fa, FiguraMusical.Negra);
+            var nota3 = new Nota(Tono.Si, FiguraMusical.Blanca);
+            this.compasCompleto.AgregarNota(nota);
+            this.compasCompleto.AgregarNota(nota2);
+            this.compasCompleto.AgregarNota(nota3);
+        }
+
+        private void CrearCompasIncompleto(TiempoCancion tiempoCancion)
+        {
+            this.compasIncompleto = new Compas(tiempoCancion);
+            var nota = new Nota(Tono.Fa, FiguraMusical.Negra);
+            var nota2 = new Nota(Tono.Fa, FiguraMusical.Negra);
+            var nota3 = new Nota(Tono.Si, FiguraMusical.Negra);
+            this.compasIncompleto.AgregarNota(nota);
+            this.compasIncompleto.AgregarNota(nota2);
+            this.compasIncompleto.AgregarNota(nota3);
         }
     }
 }
--- Source/AlgoHeroEntities/Core/Partitura.cs
+++ Source/AlgoHeroEntities/Core/Partitura.cs
@@ -1,6 +1,38 @@
-﻿namespace AlgoHero.MusicEntities.Core
+﻿using System;
+using System.Collections.Generic;
+using AlgoHero.MusicEntities.Excepciones;
+
+namespace AlgoHero.MusicEntities.Core
 {
-    class Partitura
+    public class Partitura
     {
+        private readonly TiempoCancion tiempoCancion;
+
+        private List<Compas> compases;
+        
+        public Partitura(TiempoCancion tiempoCancion)
+        {
+            this.tiempoCancion = tiempoCancion;
+            this.compases = new List<Compas>();
+        }
+
+        public int CantidadCompases
+        {
+            get { return this.compases.Count; }
+        }
+
+        public void AgregarCompas(Compas compas)
+        {
+            if (!compas.EsCompleto)
+            {
+                throw new ExcepcionCompasInvalido();
+            }
+            this.compases.Add(compas);
+        }
+
+        public Compas ObtenerCompas(int index)
+        {
+            return this.compases[index];
+        }
     }
 }
