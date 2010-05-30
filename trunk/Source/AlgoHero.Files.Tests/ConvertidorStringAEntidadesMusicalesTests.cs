73d31c31c39322ea4a641a23cff56542cbb31e74
schenkelmania <schenkelmania@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-05-30 17:21:10 +0000

    Agregue funcionalidad para leer partitura de xml

---
+++ trunk/Source/AlgoHero.Files.Tests/ConvertidorStringAEntidadesMusicalesTests.cs
@@ -0,0 +1,37 @@
+﻿using AlgoHero.MusicEntities.Enums;
+using NUnit.Framework;
+
+namespace AlgoHero.Files.Tests
+{
+    [TestFixture]
+    public class ConvertidorStringAEntidadesMusicalesTests
+    {
+        [Test]
+        public void ConvertirCadenaTonoAEnumConvierteCorrectamente()
+        {
+            Assert.AreEqual(Tono.Do, ConvertidorStringAEntidadesMusicales.ConvertirATono("do"));
+            Assert.AreEqual(Tono.Do, ConvertidorStringAEntidadesMusicales.ConvertirATono("Do"));
+            Assert.AreEqual(Tono.DoSostenido, ConvertidorStringAEntidadesMusicales.ConvertirATono("Do#"));
+            Assert.AreEqual(Tono.Re, ConvertidorStringAEntidadesMusicales.ConvertirATono("Re"));
+            Assert.AreEqual(Tono.ReSostenido, ConvertidorStringAEntidadesMusicales.ConvertirATono("Re#"));
+            Assert.AreEqual(Tono.Mi, ConvertidorStringAEntidadesMusicales.ConvertirATono("Mi"));
+            Assert.AreEqual(Tono.Fa, ConvertidorStringAEntidadesMusicales.ConvertirATono("Fa"));
+            Assert.AreEqual(Tono.FaSostenido, ConvertidorStringAEntidadesMusicales.ConvertirATono("Fa#"));
+            Assert.AreEqual(Tono.Sol, ConvertidorStringAEntidadesMusicales.ConvertirATono("Sol"));
+            Assert.AreEqual(Tono.La, ConvertidorStringAEntidadesMusicales.ConvertirATono("La"));
+            Assert.AreEqual(Tono.LaSostenido, ConvertidorStringAEntidadesMusicales.ConvertirATono("La#"));
+            Assert.AreEqual(Tono.Si, ConvertidorStringAEntidadesMusicales.ConvertirATono("Si"));
+        }
+
+        [Test]
+        public void ConvertirCadenaFiguraAEnumConvierteCorrectamente()
+        {
+            Assert.AreEqual(FiguraMusical.Redonda, ConvertidorStringAEntidadesMusicales.ConvertirAFigura("Redonda"));
+            Assert.AreEqual(FiguraMusical.Blanca, ConvertidorStringAEntidadesMusicales.ConvertirAFigura("blanca"));
+            Assert.AreEqual(FiguraMusical.Blanca, ConvertidorStringAEntidadesMusicales.ConvertirAFigura("Blanca"));
+            Assert.AreEqual(FiguraMusical.Negra, ConvertidorStringAEntidadesMusicales.ConvertirAFigura("Negra"));
+            Assert.AreEqual(FiguraMusical.Corchea, ConvertidorStringAEntidadesMusicales.ConvertirAFigura("Corchea"));
+            Assert.AreEqual(FiguraMusical.Semicorchea, ConvertidorStringAEntidadesMusicales.ConvertirAFigura("Semicorchea"));
+        }
+    }
+}
--- trunk/Source/AlgoHero.Files.Tests/ProveedorCancionXmlFixture.cs
+++ trunk/Source/AlgoHero.Files.Tests/ProveedorCancionXmlFixture.cs
@@ -3,6 +3,7 @@ using AlgoHero.Files.Interfaces;
 using AlgoHero.MusicEntities.Core;
 using System.IO;
 using System;
+using AlgoHero.MusicEntities.Enums;
 
 namespace AlgoHero.Files.Tests
 {
@@ -12,16 +13,68 @@ namespace AlgoHero.Files.Tests
     [TestFixture]
     public class ProveedorCancionXmlFixture
     {
+        private Cancion cancion;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            string pathCancion = Path.Combine(Environment.CurrentDirectory,
+            Path.Combine("Archivos Prueba", "WeWillRockYou.xml"));
+            IProveedorCancion proveedor = new ProveedorCancionXml();
+            this.cancion = proveedor.ObtenerCancion(pathCancion);
+        }
+
         [Test]
         public void ObtenerCancionDeArchivoDevuelveCancionConNombreYAutorCorrectos() 
         {
-            string pathCancion = Path.Combine(Environment.CurrentDirectory, 
-            Path.Combine("Archivos Prueba", "WeWillRockYou.xml"));
-            IProveedorCancion proveedor = new ProveedorCancionXml();
-            Cancion cancion = proveedor.ObtenerCancion(pathCancion);
-
             Assert.AreEqual("We will rock you", cancion.Nombre);
             Assert.AreEqual("Queen", cancion.Autor);
         }
+
+        [Test]
+        public void ObtenerCancionDeArchivoDevuelveCancionConTiempoCorrecto()
+        {
+            Assert.AreEqual(4, cancion.Partitura.DuracionCompas);
+            Assert.AreEqual(2, cancion.Partitura.CantidadBlancas);
+        }
+
+        [Test]
+        public void ObtenerCancionDeArchivoDevuelveCancionConCantidadDeCompasesCorrecta()
+        {
+            Assert.AreEqual(2, cancion.Partitura.CantidadCompases);
+        }
+
+        [Test]
+        public void ObtenerCancionDeArchivoDevuelveCompasesConNotasCorrectas()
+        {
+            Nota nota;
+            //Primer nota primer compas
+            nota = cancion.Partitura.ObtenerCompas(0).ObtenerNota(0);
+            Assert.AreEqual(Tono.Do, nota.Tono);
+            Assert.AreEqual(FiguraMusical.Blanca, nota.Figura);
+
+            //Segunda nota primer compas
+            nota = cancion.Partitura.ObtenerCompas(0).ObtenerNota(1);
+            Assert.AreEqual(Tono.Re, nota.Tono);
+            Assert.AreEqual(FiguraMusical.Negra, nota.Figura);
+
+            //Tercer nota primer compas
+            nota = cancion.Partitura.ObtenerCompas(0).ObtenerNota(2);
+            Assert.AreEqual(Tono.La, nota.Tono);
+            Assert.AreEqual(FiguraMusical.Negra, nota.Figura);
+
+            //Primeras cuatro notas segundo compas
+            for (int i = 0; i < 4; i++)
+            {
+                nota = cancion.Partitura.ObtenerCompas(1).ObtenerNota(i);
+                Assert.AreEqual(Tono.Mi, nota.Tono);
+                Assert.AreEqual(FiguraMusical.Corchea, nota.Figura);
+            }
+
+            nota = cancion.Partitura.ObtenerCompas(1).ObtenerNota(4);
+            Assert.AreEqual(Tono.Do, nota.Tono);
+            Assert.AreEqual(FiguraMusical.Blanca, nota.Figura);
+        }
+
     }
 }
