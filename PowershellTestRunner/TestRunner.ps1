d9e28ce32502a53052602f6dbb81dcca01f9ac40
schenkelmania <schenkelmania@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-06-01 02:10:15 +0000

    Actualice el test runner para que solo muestre datos relevantes y con colores informativos

--- PowershellTestRunner/TestRunner.ps1
+++ PowershellTestRunner/TestRunner.ps1
@@ -1,13 +1,34 @@
+
+function printResultsFromFile($filePath)
+{
+    gc $filePath | foreach{
+        if ($_.Contains("Tests run:")){
+            if ($_.Contains("Failures: 0")){
+                Write-Host $_ -foregroundcolor green
+            }
+            else
+            {
+                Write-Host $_ -foregroundcolor red
+            }
+        }
+    }
+}
+
+
 $programPath = pwd
 $nunit = "C:\Program Files (x86)\NUnit 2.4.8\bin"
 cd..
 $path = pwd
 cd $nunit
+$outputPath = [System.IO.Path]::Combine($programPath, "results.txt")
 dir $path -filter *Tests.dll -recurse | foreach{
     if ($_.FullName.contains("bin")){
         $file = [string]::format("Running {0}", $_.Name)
-        Write-Host $file -foregroundcolor red
-        .\nunit-console.exe $_.FullName
+        Write-Host $file -foregroundcolor cyan
+        .\nunit-console.exe $_.FullName | Out-File $outputPath
+        printResultsFromFile $outputPath
     }
 }
+
+del $outputPath
 cd $programPath
