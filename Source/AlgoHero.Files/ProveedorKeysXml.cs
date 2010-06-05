b6fc4db5edd8d6aad6aefca889966fb3917897bf
schenkelmania <schenkelmania@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-06-05 21:38:43 +0000

    Documente la parte que habia armado de la lectura de archivos

--- Source/AlgoHero.Files/ProveedorKeysXml.cs
+++ Source/AlgoHero.Files/ProveedorKeysXml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Windows.Input;
 using System.Xml;
 
@@ -49,7 +47,5 @@ namespace AlgoHero.Files
             }
             return listaKey;
         }
-
-
     }
 }
