94da2a7bb50fe69b5bd83b88a4a905ff9fc6c076
schenkelmania <schenkelmania@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-06-05 19:54:37 +0000

    Refectoring del iterador para hacer el codigo mas claro.

--- Source/AlgoHeroEntities/Core/Partitura.cs
+++ Source/AlgoHeroEntities/Core/Partitura.cs
@@ -59,13 +59,16 @@ namespace AlgoHero.MusicEntities.Core
 
         private class IteradorPartirura : IIterador<Nota>
         {
+
+            #region Atributos
             private Partitura partitura;
             private Compas compasActual;
             private Nota notaActual;
             private int numeroCompas;
             private int numeroNota;
             private bool tieneSiguiente;
-            
+            #endregion
+
             public IteradorPartirura(Partitura partitura)
             {
                 this.partitura = partitura;
@@ -80,6 +83,7 @@ namespace AlgoHero.MusicEntities.Core
                 }
             }
 
+            #region Interfaz Iterador
             public bool TieneSiguiente
             {
                 get { return tieneSiguiente; }
@@ -93,30 +97,55 @@ namespace AlgoHero.MusicEntities.Core
                 }
                 Nota notaADevolver = this.notaActual;
 
-                
-                //TODO: Refactor this
-                if (this.numeroNota + 1 < this.compasActual.CantidadNotas)
+                AvanzarNota();
+
+                return notaADevolver;
+            }
+            #endregion
+
+            #region Private
+            private void AvanzarNota()
+            {
+                if (!this.EnFinDeCompas())
                 {
-                    this.numeroNota += 1;
-                    this.notaActual = this.compasActual.ObtenerNota(this.numeroNota);
+                    AvanzarNotaEnMismoCompas();
                 }
-                //se acabo el compas
-                else if (this.numeroCompas + 1 < this.partitura.CantidadCompases)
+                else if (!this.EnFinDePartitura())
                 {
                     //TODO: Move to next compas
-                    this.numeroNota = 0;
-                    this.numeroCompas += 1;
-                    this.compasActual = this.partitura.ObtenerCompas(this.numeroCompas);
-                    this.notaActual = this.compasActual.ObtenerNota(this.numeroNota);
+                    AvanzarNotaEnCompasSiguiente();
                 }
-                //se acabo la partitura
                 else
                 {
                     this.tieneSiguiente = false;
                 }
-
-                return notaADevolver;
             }
+
+            private bool EnFinDePartitura()
+            {
+                return this.numeroCompas + 1 >= this.partitura.CantidadCompases;
+            }
+
+            private bool EnFinDeCompas()
+            {
+                return this.numeroNota + 1 >= this.compasActual.CantidadNotas;
+            }
+
+            private void AvanzarNotaEnCompasSiguiente()
+            {
+                this.numeroNota = 0;
+                this.numeroCompas += 1;
+                this.compasActual = this.partitura.ObtenerCompas(this.numeroCompas);
+                this.notaActual = this.compasActual.ObtenerNota(this.numeroNota);
+            }
+
+            private void AvanzarNotaEnMismoCompas()
+            {
+                this.numeroNota += 1;
+                this.notaActual = this.compasActual.ObtenerNota(this.numeroNota);
+            }
+            #endregion
+
         }
     }
 }
