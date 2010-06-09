293ba700787a9bf639f475488209c23b68a10630
manuel.soldini <manuel.soldini@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-06-09 01:43:37 +0000

    EstrategiaNivelFacil IEstrategiaNivel y sus tests... y mas...

--- trunk/PythonTestRunner/winTests.py
+++ trunk/PythonTestRunner/winTests.py
@@ -11,6 +11,7 @@ def cmd(comando):
 	try:
 		return f[0].read()
 	except:
+		
 		print "Imposible de ejecutar Comando: <"+comando+">"
 		print 
 		raw_input("Nos vemos luego, enter para salir")
@@ -57,7 +58,10 @@ def filtrar(texto, archivo, i=0):
 				if "Failures: 0" in linea:
 					return cadena+"OK"
 				else:
+					os.system("color C && echo ")
 					return cadena+" ERROR!!!!"	
+
+		os.system("color C && echo ")
 		return cadena + "FALLO"
 	except:
 		print "Saliendo: 2"
@@ -75,27 +79,43 @@ def ejecutar(archivos,PATHN):
 		i += 1
 
 def ayuda():
+	os.system("cls")
+	os.system("color 9 && echo .")
+	print
 	print "Si no funciona puede deberse al path de nunit"
 	print "revise y modifique la variable PATHN en la 4ta linea"
 	print "desde el final del documento; o descomente la 2da "
 	print "(desde el final) para realizar una busqueda;"
 	print "WARNING: esto puede demorar varios minutos, se aconseja "
 	print "tomar nota del path y modificarlo para futuras ejecuciones)"
+	print
 
 def main(PATHN):
 	print ">Path nunit= "+PATHN+"\n\n"
-	tests = cargar()
-	print "\n",len(tests),"Tests Detectados;\nCorriendo los Tests: aguarde...\n"
-	ejecutar(tests, PATHN)
-	help = raw_input("\n\nTERMINADO! (enter para salir; help y enter para ayuda)")
-	if "help" in help:
-		ayuda()
-		raw_input("\nFIN! (enter para salir)")
+	estado = True
+	while estado:
+		os.system("color A && echo .")
+		tests = cargar()
+		print "\n",len(tests),"Tests Detectados;\nCorriendo los Tests: aguarde...\n"
+		ejecutar(tests, PATHN)
+		help = raw_input("\n\nTERMINADO! (enter para salir; help y enter para ayuda, 0 y enter para repetir.)")
+		if "help" in help:
+			ayuda()
+			help = raw_input("\nFIN! (enter para salir, 0 y enter para repetir)")
+		if "0" in help:
+			estado = True
+			os.system("cls")			
+
+			print "Recargando ambiente y rests..."
+		else:
+			estado = False
 	
 
 if __name__ == "__main__":
 	os.system("cls")
+
 	print "--_Bienvenidos_--"
+
 	print "\nInciando ambiente y buscando tests...\n\n"
 	
 	# ATENCIOOOONN!!!
