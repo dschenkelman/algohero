3d5de70f771e6b393ab92a83ab8089322fa3b4d3
manuel.soldini <manuel.soldini@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-06-09 04:17:05 +0000

    Todos Los niveles actualizados (AsignarTonos no devuelve nada y modifica el controlador, pero esto no esta tan mal por ser una estrategia q solo existe para este modelo)

---
+++ trunk/Source/AlgoHero.Nivel.Tests/Core/MockControladorTeclas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using AlgoHero.Interface;
+using AlgoHero.Interface.Enums;
+
+namespace AlgoHero.Nivel.Tests.Core
+{
+    public class MockControladorTeclas : IControladorTeclas
+    {
+        private List<ITecla> Teclas;
+
+        public MockControladorTeclas()
+        {
+            this.Teclas = new List<ITecla>();
+            this.Teclas.Add(new MockTecla(1));
+            this.Teclas.Add(new MockTecla(2));
+            this.Teclas.Add(new MockTecla(3));
+            this.Teclas.Add(new MockTecla(4));
+        }
+
+        public int CantidadTeclas
+        {
+            get { return 4; }
+        }
+
+        public ITecla ObtenerTecla(int index)
+        {
+            return this.Teclas[index];
+        }
+
+        public IEnumerable<ITecla> ObtenerTeclas()
+        {
+            return this.Teclas;
+        }
+
+        public ITecla ObtenerTecla(EntidadEntrada entidadEntrada)
+        {
+            return this.Teclas[entidadEntrada.Codigo - 1];
+        }
+    }
+
+
+}
