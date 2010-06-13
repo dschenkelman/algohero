deae0989d6c2f061657d996ab4d52c565187f3a5
schenkelmania <schenkelmania@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-06-13 01:05:33 +0000

    Agregue vista de menu principal y ventana principal. Ahora voy a hacer que se muestren los niveles y canciones que se pueden elegir.

---
+++ Source/AlgoHero.Pantallas.Tests/VentanaPrincipalViewModelTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Windows.Controls;
+using NUnit.Framework;
+
+namespace AlgoHero.Pantallas.Tests
+{
+    [TestFixture]
+    public class VentanaPrincipalViewModelTests
+    {
+        private int idObtenido = 0;
+        private int idEsperado = 1;
+
+        /* Al corrector: Es bien feo el formato de este test. El problema es que necesito que cree un control y eso solo se puede 
+         * hacer en un STA thread. Como NUnit corre en MTA (no como MSTest), es la forma posible de crear el objeto 
+         * y ver que obtener el Id.
+         * De todas formas, si se les ocurre alguna forma mejor estaria bueno escucharlo, y sino aunque sea
+         * usamos lo que aprendimos de threading.
+         */
+        [Test]
+        public void CrearViewModelConControlSeteaContenido()
+        {
+            Thread t = new Thread(this.CorrerTestReal);
+            t.TrySetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
+
+            Assert.AreEqual(idEsperado, idObtenido);
+        }
+
+        private void CorrerTestReal()
+        {
+            Control control = new Control();
+            idEsperado = control.GetHashCode();
+
+            idObtenido =  new VentanaPrincipalViewModel(control).Contenido.GetHashCode();
+        }
+
+    }
+}
---
+++ Source/AlgoHero.Pantallas/VentanaPrincipal.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace AlgoHero.Pantallas
+{
+    /// <summary>
+    /// Interaction logic for VentanaPrincipal.xaml
+    /// </summary>
+    public partial class VentanaPrincipal : Window
+    {
+        public VentanaPrincipal(VentanaPrincipalViewModel model)
+        {
+            this.DataContext = model;
+            InitializeComponent();
+        }
+    }
+}
