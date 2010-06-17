2ae3b1eb82a56f84e96a4537d47b27112f4c0f8f
manuel.soldini <manuel.soldini@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-06-17 02:22:23 +0000

    subi el ejemplo q hizo dami hoy...

---
+++ Spikes/WpfApplication2/WpfApplication2/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Window1 w = new Window1(new WindowViewModel());
+            w.Show();
+        }
+    }
+}
