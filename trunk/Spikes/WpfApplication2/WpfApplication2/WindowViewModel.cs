486a2c172e4449ac16087f40157af2f6dd4e1df8
manuel.soldini <manuel.soldini@527cd883-67e5-2c66-d2c5-c5d208108475>
2010-06-17 02:22:23 +0000

    subi el ejemplo q hizo dami hoy...

---
+++ trunk/Spikes/WpfApplication2/WpfApplication2/WindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace WpfApplication2
+{
+    public class WindowViewModel : INotifyPropertyChanged
+    {
+        public WindowViewModel()
+        {
+            this.multiplicador = 1;
+        }
+
+        private int multiplicador;
+
+        public int Multiplicador 
+        {
+            get
+            {
+                return multiplicador;
+            }
+            set
+            {
+                if (multiplicador != value)
+                {
+                    this.multiplicador = value;
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("Multiplicador"));
+                }
+            } 
+        }
+
+        #region Miembros de INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
